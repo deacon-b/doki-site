216836d66f310e20d59947f31d6166d536d7eb41
ahmadhaydar <alkaf.haydar@gmail.com>
2024-02-15 22:58:19 +0700

    fix: still trying to fix it

--- src/app/components/UpcomingLivestreamCard.js
+++ src/app/components/UpcomingLivestreamCard.js
@@ -1,11 +1,13 @@
 'use client';
 
 export const UpcomingLivestreamCard = ({ video, ...props }) => {
+  const [date, ] = useState(video.available_at);
+
   return (
     <div className="card bg-base-100 shadow-xl w-42 text-base-content" {...props}>
       <div className="card-body">
         <h2 className="card-title text-sm">{video.title}</h2>
-        <p>{new Date(video.available_at).toLocaleString()} 📅</p>
+        <p>{new Date(date).toLocaleString()} 📅</p>
       </div>
     </div>
   );
