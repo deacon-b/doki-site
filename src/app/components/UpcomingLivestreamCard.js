a53e89e71a23d62ce3368e9d64ae16828cbae962
J3romey <jerome.adviento@hotmail.com>
2024-02-15 19:50:25 -0700

    stylize upcoming streams more

--- src/app/components/UpcomingLivestreamCard.js
+++ src/app/components/UpcomingLivestreamCard.js
@@ -24,12 +24,23 @@ export const UpcomingLivestreamCard = ({ video, ...props }) => {
       <div className="card-body relative overflow-hidden">
         <h2 className="font-black w-4/5 text-nowrap truncate text-xl mix-blend-difference text-yellow-700">{video.title}</h2>
 
-        <p className="absolute -rotate-[10deg] right-4 top-4 font-black text-6xl mix-blend-soft-light">
-          {monthNames[streamMonth]} {streamDay},  {streamYear}
+        <p className="absolute -rotate-[20deg] left-0 top-[-10%] font-black text-8xl mix-blend-soft-light">
+          {streamYear}
         </p>
 
-        <p className="absolute -rotate-[10deg] right-1 top-11 font-black text-4xl mix-blend-hard-light">
-        {streamTime}
+        <p className="absolute rotate-[30deg] left-[50%] top-[-10%] font-black text-8xl mix-blend-soft-light">
+          /
+        </p>
+        <p className="absolute -rotate-[60deg] left-[50%] top-[-10%] font-black text-8xl mix-blend-soft-light">
+          /
+        </p>
+
+        <p className="absolute -rotate-[20deg] right-16 top-4 font-black text-6xl mix-blend-soft-light">
+          {monthNames[streamMonth]} {streamDay}
+        </p>
+
+        <p className="absolute -rotate-[20deg] right-1 top-11 font-black text-4xl mix-blend-hard-light">
+          {streamTime}
         </p>
       </div>
     </div>
