412dd648ba7b471a17b964d88c25f4bf6032f21b
DeaconBeacon <78914402+deacon-b@users.noreply.github.com>
2024-02-16 22:31:52 +0700

    Update LatestLivestream.js

--- src/app/components/LatestLivestream.js
+++ src/app/components/LatestLivestream.js
@@ -19,7 +19,7 @@ export const LatestLivestream = async () => {
   return (
     <div style={{'--image-url': `url(${previewImg})`, 'background-size': `auto 105%`}} 
         className="card bg-neutral min-h-96 bg-[image:var(--image-url)] bg-origin-border bg-center backdrop-brightness-50">
-      <div className='card-body backdrop-brightness-75 text-neutral-content'>
+      <div className='card-body backdrop-brightness-50 text-neutral-content'>
         <h2 className="font-bold w-4/5 text-nowrap truncate text-xl">{video.title} </h2>
         <VideoStatus video={video} />
         <div className="card-actions justify-end">
