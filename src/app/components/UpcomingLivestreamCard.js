2942bf58097f8313c7e5c0d1a6ce2e9f87f1fd45
DeaconBeacon <78914402+deacon-b@users.noreply.github.com>
2024-02-20 19:52:58 +0700

    Revert Changes

--- src/app/components/UpcomingLivestreamCard.js
+++ src/app/components/UpcomingLivestreamCard.js
@@ -10,39 +10,11 @@ export const UpcomingLivestreamCard = ({ video, ...props }) => {
     setAvailableAt(date.toLocaleString());
   }, []);
 
-  const addToCalendar = () => {
-    const title = video.title;
-    const description = "Livestream with " + video.channel.name;
-    const startDateTime = video.available_at; // Assuming this is in ISO format
-    const endDateTime = ""; // You can calculate end time based on duration if available
-
-    addEventToCalendar(title, description, startDateTime, endDateTime);
-  };
-
-  const addEventToCalendar = (title, description, startDateTime, endDateTime) => {
-  // Construct the calendar event in iCalendar format
-  const calendarEvent = `BEGIN:VCALENDAR
-VERSION:2.0
-BEGIN:VEVENT
-SUMMARY:${title}
-DESCRIPTION:${description}
-DTSTART:${startDateTime}
-DTEND:${endDateTime}
-END:VEVENT
-END:VCALENDAR`;
-
-  // Create a Blob object with the calendar event content
-  const blob = new Blob([calendarEvent], { type: 'text/calendar' });
-
-  // Create a URL for the Blob object
-  const url = URL.createObjectURL(blob);
-
-  // Open the calendar URL using the webcal protocol to open the calendar application
-  window.open('webcal://' + url);
-};
-  
   return (
-    <div className="card bg-base-100 shadow-xl w-42 text-base-content" {...props} onClick={addToCalendar}>
+    <div
+      className="card bg-base-100 shadow-xl w-42 text-base-content"
+      {...props}
+    >
       <div className="card-body">
         <h2 className="card-title text-sm">{video.title}</h2>
         <p>{availableAt} 📅</p>
